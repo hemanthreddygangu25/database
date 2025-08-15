da71d4b5dda6710d44c48936a789fef8943ed717
G Hemanth Kumar Reddy <hemanthreddygangu25@gmail.com>
2025-08-16 00:15:48 +0530

    Delete 610-triangle-judgement directory

--- 610-triangle-judgement/triangle-judgement.sql
+++
@@ -1,9 +0,0 @@
-(SELECT *, 'Yes' AS triangle
-FROM Triangle
-WHERE (x + y > z) AND (y + z > x) AND (z + x > y))
-
-UNION
-
-(SELECT *, 'No' AS triangle
-FROM Triangle
-WHERE (x + y <= z) OR (y + z <= x) OR (z + x <= y));
